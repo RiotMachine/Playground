aec1adc27002dad052a41638f553063f0f4c97c6
Jacob J <jirak87@outlook.com>
2026-09-16 10:29:44 -0500

    Whipped up an iterative version of Fibonacci. O(n).

--- C++/fibonacci.cpp
+++ C++/fibonacci.cpp
@@ -12,12 +12,33 @@ int fibonacci(int element)
         return fibonacci(element-1) + fibonacci(element-2);
 }
 
+int fibonacciIt(int element)
+{
+    if (element < 0)
+        return -1;
+    else if (element == 0)
+        return 0;
+
+    int twoBack{ 0 };
+    int oneBack{ 1 };
+
+    for (int i{ 2 }; i < element; ++i)
+    {
+        int currElement{ oneBack + twoBack };
+        twoBack = oneBack;
+        oneBack = currElement;
+    }
+
+    return oneBack + twoBack;
+}
 
 int main()
 {
     constexpr int terms{ 20 };
     for (int i{ 0 }; i < terms; ++i)
-        std::cout << 'F' << i << " = " << fibonacci(i) << '\n';
+        std::cout << "Recursive F" << i << " = " << fibonacci(i) << '\n';
+    for (int i{ 0 }; i < terms; ++i)
+        std::cout << "Iterative F" << i << " = " << fibonacciIt(i) << '\n';
 
     return 0;
 }
