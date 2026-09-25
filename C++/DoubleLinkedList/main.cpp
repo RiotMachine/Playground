d895ca789b8284d759f680aff30e2d4021b78826
Jacob J <jirak87@outlook.com>
2026-09-25 06:15:01 -0500

    Wrote tests.

--- C++/DoubleLinkedList/main.cpp
+++ C++/DoubleLinkedList/main.cpp
@@ -2,13 +2,12 @@
 #include <algorithm>
 #include <iostream>
 
+// Only wrote non-const iterator, so passing LinkedList as non-const
 template <typename T>
 void print(DoubleLinkedList<T>& ll)
 {
-    std::cout << std::boolalpha 
-              << "Size: " << ll.size() << '\n'
-              << "Empty: " << ll.empty() << '\n'
-              << "Contents: ";
+    std::cout << std::boolalpha << "Size: " << ll.size()
+              << "\nEmpty: " << ll.empty() << "\nContents: ";
     for (const auto& e : ll)
         std::cout << e << ' ';
     std::cout << '\n';
@@ -16,12 +15,13 @@ void print(DoubleLinkedList<T>& ll)
 
 int main()
 {
-    DoubleLinkedList ll{ 2, 3, 4 };
+    DoubleLinkedList ll{ 2, 3, 4, 5 };
+    ll.postpend(6);
     print(ll);
     std::cout << '\n';
 
-    ll[2] = 5;
-
+    ll[0] = 1;
+    ll[3] = 6;
     ll.remove(
         std::find(ll.begin(), ll.end(), 3)
     );
